9df1838cdbb7f63b518ab1085e4001e9a8b97d1d
DominicReinmann <reinmann.dominic@bluewin.ch>
2023-07-16 12:43:54 +0200

    init commit

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static class randArray
+    {
+        public static int[] genArray(int count)
+        {
+            Random random = new Random();
+            int[] values = new int[count];
+
+            for (int i = 0; i < count; ++i)
+            {
+                values[i] = random.Next();
+            }
+            return values;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] values = randArray.genArray(2500);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+        for (int i = 0; i < values.Length; i++)
+        {
+            for (int j = 0; j < values.Length - i - 1; j++)
+            {
+                if (values[j] > values[j + 1])
+                {
+                    var tmp = values[j];
+                    values[j] = values[j + 1];
+                    values[j + 1] = tmp;
+                }
+            }
+        }
+        stopwatch.Stop();
+        TimeSpan ts = stopwatch.Elapsed;
+        System.Console.WriteLine("execution time: {0:00}:{1:00}:{2:00}.{3}",
+                                 ts.Hours, ts.Minutes, ts.Seconds, ts.Milliseconds);
+    }
+}
