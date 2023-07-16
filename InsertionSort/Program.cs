3a053fcd614fddda7fe7e0a89fc3ca233246edcb
DominicReinmann <reinmann.dominic@bluewin.ch>
2023-07-16 12:51:24 +0200

    updated proj

---
+++ InsertionSort/Program.cs
@@ -0,0 +1,38 @@
+﻿using System.Diagnostics;
+
+public class Program
+{
+    public static class randArray
+    {
+        public static int[] genArray(int count)
+        {
+            Random random = new Random();
+            int[] values = new int[count];
+
+            for (int i = 0; i < count; ++i)
+            {
+                values[i] = random.Next();
+            }
+            return values;
+        }
+    }
+
+    static void Main(string[] args)
+    {
+        int[] values = randArray.genArray(2500);
+        Stopwatch stopwatch = new Stopwatch();
+        stopwatch.Start();
+
+        // code goes here
+
+        stopwatch.Stop();
+        TimeSpan ts = stopwatch.Elapsed;
+        System.Console.WriteLine(
+            "execution time: {0:00}:{1:00}:{2:00}.{3}",
+            ts.Hours,
+            ts.Minutes,
+            ts.Seconds,
+            ts.Milliseconds
+        );
+    }
+}
