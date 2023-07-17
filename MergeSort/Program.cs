c5729ce8b4a1656e88668e52a021ac72bfd997b6
DominicReinmann <reinmann.dominic@bluewin.ch>
2023-07-17 11:09:38 +0200

    another push

--- MergeSort/Program.cs
+++ MergeSort/Program.cs
@@ -1,38 +1,76 @@
-﻿using System.Diagnostics;
+﻿public class Program {
+  public static class randArray {
+    public static int[] genArray(int count) {
+      Random random = new Random();
+      int[] values = new int[count];
 
-public class Program
-{
-    public static class randArray
-    {
-        public static int[] genArray(int count)
-        {
-            Random random = new Random();
-            int[] values = new int[count];
-
-            for (int i = 0; i < count; ++i)
-            {
-                values[i] = random.Next();
-            }
-            return values;
-        }
+      for (int i = 0; i < count; ++i) {
+        values[i] = random.Next();
+      }
+      return values;
     }
+  }
 
-    static void Main(string[] args)
-    {
-        int[] values = randArray.genArray(25000);
-        Stopwatch stopwatch = new Stopwatch();
-        stopwatch.Start();
+  public static int[] Merge(int[] left, int[] right) {
+    var values = new int[left.Length + right.Length];
+    int leftIndex = 0, rightIndex = 0, mergedIndex = 0;
 
-        // code here
-
-        stopwatch.Stop();
-        TimeSpan ts = stopwatch.Elapsed;
-        System.Console.WriteLine(
-            "execution time: {0:00}:{0:00}:{0:00}.{0}",
-            ts.Hours,
-            ts.Minutes,
-            ts.Seconds,
-            ts.Milliseconds
-        );
+    while (leftIndex < left.Length && rightIndex < right.Length) {
+      if (left[leftIndex] < right[rightIndex]) {
+        values[mergedIndex] = left[leftIndex];
+        leftIndex++;
+      } else {
+        values[mergedIndex] = right[rightIndex];
+        rightIndex++;
+      }
+      mergedIndex++;
     }
+    while (leftIndex < left.Length) {
+      values[mergedIndex] = left[leftIndex];
+      leftIndex++;
+      mergedIndex++;
+    }
+    while (rightIndex < right.Length) {
+      values[mergedIndex] = right[rightIndex];
+      rightIndex++;
+      mergedIndex++;
+    }
+    return values;
+  }
+
+  public static int[] MergeSort(int[] values) {
+    if (values.Length < 2) {
+      return values;
+    }
+    int mid = values.Length / 2;
+    int[] left = new int[mid];
+    int[] right = new int[values.Length - mid];
+    Array.Copy(values, 0, left, 0, mid);
+    Array.Copy(values, mid, right, 0, values.Length - mid);
+
+    left = MergeSort(left);
+    right = MergeSort(right);
+
+    return Merge(left, right);
+  }
+
+  static void Main(string[] args) {
+    int[] values = randArray.genArray(2500000);
+    DateTime startTime = DateTime.Now;
+
+    int[] sortedArray = MergeSort(values);
+    DateTime endTime = DateTime.Now;
+    TimeSpan elapsedTime = endTime - startTime;
+
+    string filePath = "log.txt";
+    using (StreamWriter writer = new StreamWriter(filePath)) {
+      writer.WriteLine("sortedArray");
+      writer.WriteLine("Number of entries: {0}", sortedArray.Length);
+      writer.WriteLine("Execution time: {0}", elapsedTime);
+      foreach (int num in sortedArray) {
+        writer.WriteLine(num);
+      }
+      System.Console.WriteLine("Finished");
+    }
+  }
 }
