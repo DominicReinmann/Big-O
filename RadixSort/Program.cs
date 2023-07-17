81fed35aae13ef79bf0f3c87d2c65c2c22a2ffa8
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2023-07-17 21:26:25 +0200

    radix implementet

--- RadixSort/Program.cs
+++ RadixSort/Program.cs
@@ -1,6 +1,4 @@
-﻿using System.Diagnostics;
-
-public class Program
+﻿public class Program
 {
     public static class randArray
     {
@@ -17,22 +15,71 @@ public class Program
         }
     }
 
+    public static double getDigit(int num, int place)
+    {
+        return Math.Floor(Math.Abs(num) / Math.Pow(10, place)) % 10;
+    }
+
+    public static int getNumberOfDigits(int num)
+    {
+        return (int)Math.Floor(Math.Log10(Math.Abs(10))) + 1;
+    }
+
+    public static int[] RadixSort(int[] values)
+    {
+        var maxDigit = 0;
+
+        for (int i = 0; i < values.Length; i++)
+        {
+            maxDigit = Math.Max(maxDigit, getNumberOfDigits(values[i]));
+        }
+
+        for (int i = 0; i < maxDigit; i++)
+        {
+            var buckets = new List<List<int>>();
+
+            for (int j = 0; j < 10; j++)
+            {
+                buckets.Add(new List<int>());
+            }
+
+            for (int j = 0; j < values.Length; j++)
+            {
+                var digits = (int)getDigit(values[j], i);
+                buckets[digits].Add(values[j]);
+            }
+
+            var index = 0;
+
+            for (int j = 0; j < buckets.Count; j++)
+            {
+                for (int k = 0; k < buckets[j].Count; k++)
+                {
+                    values[index++] = buckets[j][k];
+                }
+            }
+        }
+
+        return values;
+    }
+
     static void Main(string[] args)
     {
-        int[] values = randArray.genArray(25000);
+        int[] values = randArray.genArray(25000000);
         DateTime startTime = DateTime.Now;
+
+        RadixSort(values);
 
         DateTime endTime = DateTime.Now;
         TimeSpan elapsedTime = endTime - startTime;
 
-        string filePath = "Insertion.txt";
+        string filePath = "Radix.txt";
         using (StreamWriter writer = new StreamWriter(filePath))
         {
             writer.WriteLine("sortedArray");
             writer.WriteLine(values.Count() + " entrys");
             writer.WriteLine("Execution time: {0}", elapsedTime);
-
-            System.Console.WriteLine("Finished");
         }
+        System.Console.WriteLine("Finished");
     }
 }
