46970a07fa21aad586535a84376f6eab966ca8d5
Dominic Reinmann <reinmann.dominic@bluewin.ch>
2023-08-18 22:43:37 +0200

    established a baseline to work from for the new sorting algos

--- BucketSort/Program.cs
+++ BucketSort/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿public class Program
+{
+  public static class randArray
+  {
+    public static int[] genArray(int count)
+    {
+      Random random = new Random();
+      int[] values = new int[count];
+
+      for (int i = 0; i < count; ++i)
+      {
+        values[i] = random.Next();
+      }
+      return values;
+    }
+  }
+
+  static void Main(string[] args)
+  {
+    int[] values = randArray.genArray(25000000);
+    DateTime startTime = DateTime.Now;
+
+    DateTime endTime = DateTime.Now;
+    TimeSpan elapsedTime = endTime - startTime;
+
+    string filePath = "BucketSort.txt";
+    using (StreamWriter writer = new StreamWriter(filePath))
+    {
+      writer.WriteLine("sortedArray");
+      writer.WriteLine(values.Count() + " entrys");
+      writer.WriteLine("Execution time: {0}", elapsedTime);
+    }
+    System.Console.WriteLine("Finished");
+  }
+}
